b39a9b3a7bb0f9748d64000b5ba237aade458671
MatteoBenetazzo5 <matteobenetazzo5@gmail.com>
2026-02-12 19:55:06 +0100

    Preparazione cartelle compito del venerdì

---
+++ src/main/java/matteobenetazzo/testweektwobu2/TestWeekTwoBU2Application.java
@@ -0,0 +1,13 @@
+package matteobenetazzo.testweektwobu2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWeekTwoBU2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestWeekTwoBU2Application.class, args);
+    }
+
+}
---
+++ src/test/java/matteobenetazzo/testweektwobu2/TestWeekTwoBU2ApplicationTests.java
@@ -0,0 +1,13 @@
+package matteobenetazzo.testweektwobu2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestWeekTwoBU2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
